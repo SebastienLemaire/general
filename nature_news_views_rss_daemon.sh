9500ec3cc8556f4afd20775122372802d70e85ad
Sébastien Lemaire <sebastien.lemaire@fjd-ceph.org>
2025-09-30 14:51:25 +0200

    add 'ALL_RSS_feeds' section

--- nature_news_views_rss_daemon.sh
+++ nature_news_views_rss_daemon.sh
@@ -6,8 +6,20 @@ nline="$( echo "${bouh}" | grep nature_news_views_rss | wc -l )"
 if [ ${nline} -le 2 ]; then
     echo 'Launch RSS daemon.'
     while [ 1 == 1 ]; do
-        python /Users/labo/general/nature_news_views_rss.py > /dev/null 2>&1
-        sleep 21600
+        
+        echo "hostname: $(more /etc/hostname)"
+        if [ "$( more /etc/hostname )" == 'PC343' ]; then
+            conda activate bash
+            script_dir=/mnt/c/Users/slemaire/softwares/general
+        else
+            script_dir=/Users/labo/general
+        fi
+
+        python "${script_dir}"/nature_news_views_rss.py #> /dev/null 2>&1
+        echo ">>> Update on $(date)"
+
+        sleep 86400 # 24 hours
+        # sleep 5
     done
 fi
 
